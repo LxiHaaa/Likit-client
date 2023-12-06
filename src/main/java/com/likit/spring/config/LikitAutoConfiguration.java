57a78ab7b47cc6adce90f7a4393ea8c6ce13f960
LXY <2246052104@qq.com>
2023-12-06 20:11:14 +0800

    feat:project init

---
+++ src/main/java/com/likit/spring/config/LikitAutoConfiguration.java
@@ -0,0 +1,16 @@
+package com.likit.spring.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: LXY
+ * @create: 2023-12-06 19:41
+ * @Description:
+ */
+@Configuration
+@ComponentScan(basePackages = "com.likit.spring")
+public class LikitAutoConfiguration {
+
+}
---
+++ src/main/java/com/likit/spring/config/LikitProperties.java
@@ -0,0 +1,19 @@
+package com.likit.spring.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: LXY
+ * @create: 2023-12-06 19:50
+ * @Description:
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "likit")
+public class LikitProperties {
+
+    public String host;
+
+}
