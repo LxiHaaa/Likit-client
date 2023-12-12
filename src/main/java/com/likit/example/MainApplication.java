908595995347441b3432ded95dafdd84d257f80f
LXY <2246052104@qq.com>
2023-12-12 20:32:04 +0800

    feat:add example

---
+++ src/main/java/com/likit/example/MainApplication.java
@@ -0,0 +1,16 @@
+package com.likit.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: LXY
+ * @create: 2023-12-12 20:00
+ * @Description:
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
---
+++ src/main/java/com/likit/example/controller/VoteController.java
@@ -0,0 +1,40 @@
+package com.likit.example.controller;
+
+import com.likit.spring.api.LikitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: LXY
+ * @create: 2023-12-07 22:19
+ * @Description:
+ */
+@RestController
+@RequestMapping("/vote")
+public class VoteController {
+
+    @Autowired
+    private LikitService likitService;
+
+    @GetMapping("/vote")
+    public String Vote(){
+        long count = likitService.vote("BTYPE","MESSAGE","LXY");
+        return "success:" + count;
+    }
+
+    @GetMapping("/unvote")
+    public String UnVote(){
+        long count = likitService.unvote("BTYPE","MESSAGE","LXY");
+        return "success:" + count;
+    }
+
+    @GetMapping("/count")
+    public String count(){
+        long count = likitService.getVoteCount("BTYPE", "MESSAGE");
+        return "success:" + count;
+    }
+
+
+}
